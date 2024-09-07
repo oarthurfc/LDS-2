6c83648a50b4aa86b9f379e2570219b48955118c
oarthurfc <arthuryouface@gmail.com>
2024-09-07 01:05:47 -0300

    Inicializando projeto

---
+++ src/main/java/com/pucminas/LDS_2/Lds2Application.java
@@ -0,0 +1,13 @@
+package com.pucminas.LDS_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lds2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lds2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/pucminas/LDS_2/Lds2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pucminas.LDS_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lds2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
